9c95f099f44d1ab56bbabef8f222f7a78a9929b9
MasonBoom <masonboom@gmail.com>
2022-03-18 07:18:27 -0400

    finished codecademy project portion

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
+import { selectTopics } from "../features/topics/topicsSlice";
+import { addQuizThunk } from "../features/quizzes/quizzesSlice";
+import { addCard } from "../features/cards/cardsSlice";
 
 export default function NewQuizForm() {
   const [name, setName] = useState("");
   const [cards, setCards] = useState([]);
   const [topicId, setTopicId] = useState("");
   const history = useHistory();
-  const topics = {};
+  const dispatch = useDispatch();
+  const topics = useSelector(selectTopics);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,10 +22,13 @@ export default function NewQuizForm() {
     }
 
     const cardIds = [];
+    cards.forEach((card) => {
+      let cardId = uuidv4();
+      cardIds.push(cardId);
+      dispatch(addCard({ id: cardId, front: card.front, back: card.back }));
+    })
 
-    // create the new cards here and add each card's id to cardIds
-    // create the new quiz here
-
+    dispatch(addQuizThunk({ id: uuidv4(), name: name, topicId: topicId, cardIds: cardIds }));
     history.push(ROUTES.quizzesRoute());
   };
 
